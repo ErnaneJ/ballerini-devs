47ecd8985ebf7f061ed5568bd4612f2e743069ec
Ernane Ferreira <Ernane.junior25@gmail.com>
2022-02-15 18:23:43 -0300

    :tada: setup inicial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Devs from "./pages/Devs";
+import Home from "./pages/Home";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="devs" element={<Devs />} />
+        {/* <Route path="*" element={<NoPage />} /> */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Devs/index.js
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+const Devs = () => {
+  return <>
+    <h1>Devs</h1>
+    <Link to="/">Home</Link>
+  </>;
+};
+
+export default Devs;
