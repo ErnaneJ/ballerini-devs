6285e0a34b0e1d782c546e21888f59becebc8e17
Ernane Ferreira <Ernane.junior25@gmail.com>
2022-02-18 09:51:45 -0300

    Austando requests

--- src/lib/devs.js
+++ src/lib/devs.js
@@ -1,4 +1,7 @@
-const defaultAvatar = "https://thumbs.dreamstime.com/b/default-avatar-profile-icon-social-media-user-vector-image-icon-default-avatar-profile-icon-social-media-user-vector-image-209162840.jpg";
+const defaultAvatar  = "https://thumbs.dreamstime.com/b/default-avatar-profile-icon-social-media-user-vector-image-icon-default-avatar-profile-icon-social-media-user-vector-image-209162840.jpg";
+const endpointApi    = 'https://api-balle-dev.herokuapp.com'; // 'http://localhost:3001'
+const endpointGithub = 'https://api.github.com';
+
 export const devStructure = {
   "avatar": defaultAvatar,
   "name": "",
@@ -10,7 +13,7 @@ export const devStructure = {
 }
 export const getAllDevs = async (applyDevs, setLoading=null) => {
   if(setLoading) setLoading(true)
-  return await fetch('https://api-balle-dev.herokuapp.com/dev')
+  return await fetch(`${endpointApi}/dev`)
   .then(response => response.json())
   .then(data => {
     applyDevs(data)
@@ -19,7 +22,7 @@ export const getAllDevs = async (applyDevs, setLoading=null) => {
 };
 
 export const addNewDev = async (dev, updateDevs) => {
-  return await fetch('https://api-balle-dev.herokuapp.com/dev', {
+  return await fetch(`${endpointApi}/dev`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
@@ -27,26 +30,24 @@ export const addNewDev = async (dev, updateDevs) => {
     body: JSON.stringify(dev)
   }).then(response => response.json())
   .then(data => {
-    console.log(data);
     getAllDevs(updateDevs);
   }).catch(e => console.error(e))
 };
 
 export const deleteDev = async (dev, updateDevs) => {
-  return await fetch(`https://api-balle-dev.herokuapp.com/dev/${dev.id}`, {
+  return await fetch(`${endpointApi}/dev/${dev.id}`, {
     method: 'DELETE',
     headers: {
       'Content-Type': 'application/json'
     }
   }).then(response => response.json())
   .then(data => {
-    console.log(data);
     getAllDevs(updateDevs);
   }).catch(e => console.error(e))
 };
 
 export const updateDev = async (dev, updateDevs) => {
-  return await fetch(`https://api-balle-dev.herokuapp.com/dev/${dev.id}`, {
+  return await fetch(`${endpointApi}/dev/${dev.id}`, {
     method: 'PUT',
     headers: {
       'Content-Type': 'application/json'
@@ -54,13 +55,12 @@ export const updateDev = async (dev, updateDevs) => {
     body: JSON.stringify(dev)
   }).then(response => response.json())
   .then(data => {
-    console.log(data);
     getAllDevs(updateDevs);
   }).catch(e => console.error(e))
 };
 
 export const loadUserToGithub = async (dev, updateDev) => {
-  const resp = await fetch(`https://api.github.com/users/${dev.nick_github}`);
+  const resp = await fetch(`${endpointGithub}/users/${dev.nick_github}`);
   const respData = await resp.json();
   updateDev({...dev, 
     avatar: respData.avatar_url,
